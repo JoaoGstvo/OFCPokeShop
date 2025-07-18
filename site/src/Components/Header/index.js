3cb3cbafdd797920f4f989c4df869cdcbe560dcd
JoaoGstvo <joaogustavocarmomoraes@gmail.com>
2025-07-18 18:30:46 -0300

    Criação de novas pastas para organização e teste do routes

---
+++ site/src/Components/Header/index.js
@@ -0,0 +1,13 @@
+import "./index.scss";
+
+function Header() {
+    return (
+        <main className="page-cabecalho">
+            <section className="cabecalho-section">
+                teste
+            </section>
+        </main>
+    );
+}
+
+export default Header;
---
+++ site/src/Pages/LandingPage/index.js
@@ -0,0 +1,13 @@
+import "./index.scss";
+
+import Header from "../../Components/Header";
+
+function LandingPage() {
+    return (
+        <main className="page-landing">
+            <Header />
+        </main>
+    );
+}
+
+export default LandingPage;
