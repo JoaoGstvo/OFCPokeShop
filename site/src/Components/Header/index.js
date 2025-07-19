399133e336c80ae5c9841948dac4b396917fa2e9
JoaoGstvo <joaogustavocarmomoraes@gmail.com>
2025-07-19 19:16:45 -0300

    Header feito (CSS)

--- site/src/Components/Header/index.js
+++ site/src/Components/Header/index.js
@@ -2,9 +2,34 @@ import "./index.scss";
 
 function Header() {
     return (
-        <main className="page-cabecalho">
+        <main className="cabecalho-page">
             <section className="cabecalho-section">
-                teste
+                <div className="cabecalho-logo">
+                    <img className="cabecalho-image" src="/images/Logo.png" alt="" />
+                </div>
+                <div className="cabecalho-busca">
+                    <input className="cabecalho-input" type="text" placeholder="Buscar Pokémon..." />
+                </div>
+                <div className="cabecalho-opcoes">
+                    <div id="div-opcoes-hover">
+                        <a href="/">Como Funciona</a>
+                    </div>
+                    <div id="div-opcoes-hover">
+                        <a href="/">Início</a>
+
+                    </div>
+                    <div id="div-opcoes-hover">
+                        <a href="/">Pokémons</a>
+
+                    </div>
+                    <div className="cabecalho-div-carrinho">
+                        <a href="/">Carrinho</a>
+                        <span className="cabecalho-contador-carrinho">2</span>
+                    </div>
+                    <div className="cabecalho-perfil">
+                        <p>J</p>
+                    </div>
+                </div>
             </section>
         </main>
     );
